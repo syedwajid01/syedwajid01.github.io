49c12c347f3b666885493dc4d4fd2109c1787b39
Syed Wajid Dev <53843406+syedwajid01@users.noreply.github.com>
2021-03-14 09:46:50 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,8 +37,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "023b695934a7dd3c5e4fc8940647abaa",
-"/": "023b695934a7dd3c5e4fc8940647abaa",
+"index.html": "7849c4e1505d2f8a8997c3ef61d5d9d6",
+"/": "7849c4e1505d2f8a8997c3ef61d5d9d6",
 "main.dart.js": "a5a8c19ea97ec64188213a4947f3c251",
 "manifest.json": "6751b372e33e9ba5f3e2f8c37000a615",
 "version.json": "5e6e78f83baa83b057de97524201b464"
